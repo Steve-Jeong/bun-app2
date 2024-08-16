fda9491ff84537ad21e7748df1eed14d1a13bfe3
Steve Jeong <jst0930@gmail.com>
2024-08-16 17:20:13 +0300

    abcd

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 // require('dotenv').config()
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res)=> {
-  console.log('HOST_PORT : ', process.env.HOST_PORT)
-  console.log('req.ip : ', req.ip)
-	res.send('Hello World 1234')
-})
-const PORT = process.env.PORT || 3000
-app.listen(PORT,()=>{
-	console.log(`Server is running at port ${PORT}`)
-})
+app.get("/", (req, res) => {
+  console.log("HOST_PORT : ", process.env.HOST_PORT);
+  console.log("req.ip : ", req.ip);
+  console.log("hello world");
+  res.send("Hello World 1234 abcd ");
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
