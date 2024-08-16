3c73c15baa5d67500877bd3647b792e5b430960d
Steve Jeong <jst0930@gmail.com>
2024-08-16 17:16:28 +0300

    1234

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res)=> {
   console.log('HOST_PORT : ', process.env.HOST_PORT)
   console.log('req.ip : ', req.ip)
-	res.send('Hello World')
+	res.send('Hello World 1234')
 })
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
